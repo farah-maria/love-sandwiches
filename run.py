cb8747af12675d29d8664eff249b48698e363a0e
Farah M <farah.maria.rahman@gmail.com>
2022-08-02 16:08:39 +0000

    add code in run.py to calcutate average stock for each item type, adding 10%

--- run.py
+++ run.py
@@ -101,6 +101,17 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+
 
 def main():
     """
@@ -114,4 +125,7 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
+calculate_stock_data(sales_columns)
