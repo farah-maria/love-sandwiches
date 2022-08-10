f9ace8afd6beeb7229548d4cf6022ee96bfe1324
Farah M <farah.maria.rahman@gmail.com>
2022-08-10 14:31:08 +0000

    add spacing in run.py to solve red issues under warnings

--- run.py
+++ run.py
@@ -97,8 +97,8 @@ def get_last_5_entries_sales():
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-
     return columns
+
 
 def calculate_stock_data(data):
     """
@@ -111,9 +111,9 @@ def calculate_stock_data(data):
         int_column = [int(num) for num in column]
         average = sum(int_column) / len(int_column)
         stock_num = average * 1.1
-        new_stock_data.append(round(stock_num))
-    
+        new_stock_data.append(round(stock_num))    
     return new_stock_data
+
 
 def main():
     """
@@ -127,6 +127,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
